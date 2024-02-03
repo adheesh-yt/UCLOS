cc5164e661169a7eafa2a711e1b0bd6aca798882
adheesh-yt <142108798+adheesh-yt@users.noreply.github.com>
2024-02-03 23:42:42 +0530

    Add files via upload

---
+++ Command_Files/Cat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UCLOS.Command_Files;
+
+namespace OSEEG
+{
+    public class Cat : UCLOSCMD
+    {
+
+        public Cat(string name) : base(name) { }
+
+        public override string execute(string[] args)
+        {
+            Console.WriteLine(@"
+             ............
+              /\_/\  
+             ( owo ) - mow
+              (   ""  )
+               -_-_-_-   
+");
+            return "";
+
+        }
+
+    }
+}
---
+++ Command_Files/ana.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UCLOS.Command_Files;
+
+namespace OSEEG
+{
+    public class Ana : UCLOSCMD
+    {
+
+        public Ana(string name) : base(name) { }
+
+        public override string execute(string[] args)
+        {
+            Console.WriteLine(@"
+             O  O
+            /|/\|/
+            / \/ \
+");
+            Console.WriteLine("The People Who Made this - Adheesh and Aadvik");
+            return "";
+
+        }
+
+    }
+}
